85acc353ba959cd88d258df7b4bcc6380c740b9b
Kelly Christina <keely06@live.com>
2019-06-11 23:50:29 -0300

    repositorio

---
+++ server-steven-universe/StevenRepository.js
@@ -0,0 +1,85 @@
+const cartoon = {
+    results: [
+        {
+            name: 'Amethyst',
+            nickname: 'Runt',
+            species: 'Gem',
+            weapons: 'Whip, Modern Gem Technology', 
+            sex: 'Sexless',
+            gender: 'She/Her',
+            traits: {
+                hair: 'Pele Lavender',
+                eyes: 'Dark Indigo',
+                gemstone: 'Amethyst',
+                gemType: 'Quartz'
+            },
+            professionalStatus: {
+                affiliations: {
+                    gemHomeworld: 'Pink Diamond, Steven'
+                },
+                ocuppations: 'Guardian, Champion Wrestler',
+                alignment: 'Crystal Gems' 
+            },
+            img: {
+                url: 'https://vignette.wikia.nocookie.net/steven-universe/images/7/70/Jfek.png/revision/latest/scale-to-width-down/350?cb=20151130211342.png'
+            }
+            
+        },
+
+        {
+            name: 'Pearl',
+            nickname: 'P, My Pearl',
+            species: 'Gem',
+            weapons: 'Spear, Many Swords', 
+            sex: 'Sexless',
+            gender: 'She/Her',
+            traits: {
+                hair: 'Peach',
+                eyes: 'Sky Blue',
+                gemstone: 'Pearl',
+                gemType: 'Pearl'
+            },
+            professionalStatus: {
+                affiliations: {
+                    gemHomeworld: 'Pink Diamond'
+                },
+                ocuppations: 'Servant, Guardian',
+                alignment: 'Crystal Gems' 
+            },
+            img: {
+                url: 'https://vignette.wikia.nocookie.net/steven-universe/images/3/38/CYM_Pearl_Request_by_RylerGamerDBS.png/revision/latest/scale-to-width-down/305?cb=20190502165900.png'
+            }
+            
+        },
+
+        {
+            name: 'Garnet',
+            nickname: 'Perma-fusion, Power Coucple, G-Squad, The Boss, Captain Square',
+            species: 'Gem',
+            weapons: 'Gauntlets', 
+            sex: 'Sexless',
+            gender: 'She/Her',
+            traits: {
+                hair: 'Indigo Black',
+                eyes: 'Left: Blue, Middle: Violet, Right: Red',
+                gemstone: 'Garnet',
+                gemType: 'Fusion'
+            },
+            professionalStatus: {
+                affiliations: {
+                    gemHomeworld: 'Off Colors'
+                },
+                ocuppations: 'Guardian, De Facto Leader of Crystal Gems, Big Donut',
+                alignment: 'Crystal Gems' 
+            },
+            img: {
+                url: 'https://vignette.wikia.nocookie.net/steven-universe/images/c/cf/Garnet-NewGen_2.png/revision/latest/scale-to-width-down/287?cb=20190208021043.png'
+            }
+            
+        }
+
+
+    ]
+}
+
+module.exports = { cartoon }
