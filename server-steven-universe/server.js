b6d064067b55941da56f654ab85358c2bc042cf3
Kelly Christina <keely06@live.com>
2019-06-11 23:52:47 -0300

    metodo get

--- server-steven-universe/server.js
+++ server-steven-universe/server.js
@@ -6,3 +6,18 @@ const controller = require('./StevenController');
 const server = express();
 server.use(cors());
 server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+    res.send(controller.getAll());
+})
+
+// server.post('/', (req, res) => {
+//     res.send(201)
+// })
+
+// server.delete('/users/:id',(req, res) => {
+//     res.sendStatus(204);
+// })
+
+server.listen(3000)
+console.log('servidor rodando na porta 3010');
